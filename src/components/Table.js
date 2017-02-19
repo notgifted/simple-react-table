bb211722a331cf91e8879f6767d6a351520a1483
Vladimir Kulishin <grivoos@yandex.com>
2017-02-19 21:04:19 +0400

    add classnames to options

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 export default class Table extends React.Component {
   render() {
-    const data = this.props.data;
+    const {data, options} = this.props;
     const heads = data.length ? Object.keys(data[0]) : [];
 
     const headRow = heads.map((head, index) =>
-      <th key={index}>{head}</th>
+      <th className={options.headCellClassName} key={index}>{head}</th>
     );
 
     const dataRows = data.map((item, index) => {
       const dataRowIndex = this.props.dataRowPrimaryKey ? item[this.props.dataRowPrimaryKey] : index;
 
       return (
-        <tr key={dataRowIndex}>
+        <tr className={options.rowClassName} key={dataRowIndex}>
           {
             heads.map((head, index) =>
-              <td key={index}>{item[head]}</td>
+              <td className={options.rowCellClassName} key={index}>{item[head]}</td>
             )
           }
         </tr>
@@ -24,13 +24,13 @@ export default class Table extends React.Component {
     });
 
     return (
-      <table>
-        <thead>
-          <tr>
+      <table className={options.tableClassName}>
+        <thead className={options.headClassName}>
+          <tr className={options.headRowClassName}>
             {headRow}
           </tr>
         </thead>
-        <tbody>
+        <tbody className={options.bodyClassName}>
           {dataRows}
         </tbody>
       </table>
@@ -41,13 +41,27 @@ export default class Table extends React.Component {
 Table.propTypes = {
   data: React.PropTypes.array,
   options: React.PropTypes.shape({
-    dataRowPrimaryKey: React.PropTypes.string
+    dataRowPrimaryKey: React.PropTypes.string,
+    tableClassName: React.PropTypes.string,
+    headClassName: React.PropTypes.string,
+    headRowClassName: React.PropTypes.string,
+    headCellClassName: React.PropTypes.string,
+    bodyClassName: React.PropTypes.string,
+    rowClassName: React.PropTypes.string,
+    rowCellClassName: React.PropTypes.string
   })
 };
 
 Table.defaultProps = {
   data: [],
   options: {
-    dataRowPrimaryKey: ''
+    dataRowPrimaryKey: '',
+    tableClassName: '',
+    headClassName: '',
+    headRowClassName: '',
+    headCellClassName: '',
+    bodyClassName: '',
+    rowClassName: '',
+    rowCellClassName: ''
   }
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import Table from './components/Table';
 import users from './data/users';
 
 const options = {
-  dataRowPrimaryKey: '_id'
+  dataRowPrimaryKey: '_id',
+  tableClassName: 'simple-table',
+  headClassName: 'simple-table-head',
+  headRowClassName: 'simple-table-head-row',
+  headCellClassName: 'simple-table-header',
+  bodyClassName: 'simple-table-body',
+  rowClassName: 'simple-table-row',
+  rowCellClassName: 'simple-table-cell'
 };
 
 ReactDOM.render(
